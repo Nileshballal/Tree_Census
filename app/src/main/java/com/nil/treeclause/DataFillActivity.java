ed3a68ad4194a8de2c808eaf19d0161829d110ed
Nileshballal <nilesh9921@gmail.com>
2022-03-24 10:58:03 +0530

    By Nilesh

---
+++ app/src/main/java/com/nil/treeclause/DataFillActivity.java
@@ -0,0 +1,17 @@
+package com.nil.treeclause;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DataFillActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tree_details_add_lay);
+
+    }
+}
